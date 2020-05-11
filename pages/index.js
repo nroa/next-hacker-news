80ebef4d630c04838a265cd64602cea4cf96393f
normRoa <normanroam@gmail.com>
2020-05-10 19:19:40 -0700

    Styling the News Stories

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,14 @@
 import fetch from "isomorphic-fetch";
 import Error from "next/error";
+import StoryList from '../components/StoryList'
 
 class Index extends React.Component {
   static async getInitialProps() {
     let stories;
     try {
-      const response = await fetch("https://node-hnapi.herokuapp.com/news?page=1");
+      const response = await fetch(
+        "https://node-hnapi.herokuapp.com/news?page=1"
+      );
       stories = await response.json();
     } catch (err) {
       console.log(err);
@@ -17,17 +20,15 @@ class Index extends React.Component {
   render() {
     const { stories } = this.props;
 
-    if(stories.length === 0) {
-      return <Error statusCode={503} />
+    if (stories.length === 0) {
+      return <Error statusCode={503} />;
     }
-    return <div>
+    return (
+      <div>
         <h1>hacker next</h1>
-        <div>
-          {stories.map(story => (
-            <h2 key={story.id}>{story.title}</h2>
-          ))}
-        </div>
-      </div>;
+        <StoryList stories={stories} />
+      </div>
+    );
   }
 }
 
